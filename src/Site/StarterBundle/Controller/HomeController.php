f61a39374153962de7484803a0cd738c4a967a0b
Kylian Alazard <alazardandroid@gmail.com>
2015-06-25 23:18:27 +0200

    Creation du controleur pour la page d'accueil du site (vide)

--- src/Site/StarterBundle/Controller/HomeController.php
+++ src/Site/StarterBundle/Controller/HomeController.php
@@ -1,7 +1,13 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alazard
- * Date: 25/06/15
- * Time: 23:13
- */
+
+namespace Site\StarterBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('SiteStarterBundle:Home:index.html.twig');
+    }
+}
